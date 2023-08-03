a8d37e19104ba836ec2bb8e9c464bc47db39528b
Frank Kilcommins <frank.kilcommins@smartbear.com>
2023-08-03 14:50:21 +0100

    switching to custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://smartbear-devrel.github.io',
-    base: '/problems-registry',
+    site: 'problems-registry.smartbear.com',
 });
