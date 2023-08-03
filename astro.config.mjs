089d39e6e6451cdd405d9354a7d49e23251714ab
Frank Kilcommins <frank.kilcommins@smartbear.com>
2023-08-03 11:24:23 +0100

    removing cname

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://problems-registry.smartbear.com',
+    site: 'https://smartbear-devrel.github.io',
+    base: '/problems-registry',
 });
