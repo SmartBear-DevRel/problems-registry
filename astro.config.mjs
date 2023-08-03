5c52f33be0b617aef5782590b4e80942eeea1620
Frank Kilcommins <frank.kilcommins@smartbear.com>
2023-08-03 15:07:29 +0100

    adding base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://problems-registry.smartbear.com',
+    base: '/problems-registry'
 });
