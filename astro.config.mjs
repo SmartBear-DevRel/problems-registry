bb3ce93bb02cd64bc9e48df24c3a7039036c256b
Frank Kilcommins <frank.kilcommins@smartbear.com>
2023-08-04 10:26:39 +0100

    remove base config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,4 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://problems-registry.smartbear.com',
-    base: '/problems-registry'
 });
